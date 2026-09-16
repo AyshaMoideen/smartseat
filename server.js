2945a845d5a50cbb2d1d639c34cd70ef8482cbef
[aysha afnan] <[ayshaafnan.in@gmail.com]>
2026-09-17 03:07:29 +0530

    Fix Express deployment detection

--- server.js
+++ server.js
@@ -42,10 +42,8 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-if (require.main === module) {
-    app.listen(PORT, () => {
-        console.log(`🚀 Server running on http://localhost:${PORT}`);
-    });
-}
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
 
 module.exports = app;
