b42ed157d2188712ac21f590fbde286113187f4a
[aysha afnan] <[ayshaafnan.in@gmail.com]>
2026-09-18 20:10:54 +0530

    Fix production teacher login API

--- frontend/js/teacher-login.js
+++ frontend/js/teacher-login.js
@@ -83,7 +83,7 @@ form.addEventListener("submit", async function (e) {
 
     try {
 
-        const response = await fetch("http://localhost:5000/api/auth/login", {
+        const response = await fetch("/api/auth/login", {
 
             method: "POST",
 
